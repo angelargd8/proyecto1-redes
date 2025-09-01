6e14b79fedca0cd91c812426ff853e8071d71819
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2025-08-31 22:11:54 -0600

    feat: add client with openia

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Proyecto 1 : Uso de protocolo existente
+# Projet 1 : Use of existing protocol
+
+# Description:
+
+# Features: 
+
+
+crear el entorno:
+
+```
+python3 -m venv .venv
+```
+
+activarlo:
+
+```
+source .venv/bin/activate
+```
+
+instalar depedencias:
+
+```
+pip install anthropic dotenv openai
+```
+
+# References: 
+- https://modelcontextprotocol.io/quickstart/server
+
+the next reference I know that is about claude. First I tried, but when I saw that I had to pay, preffer openia. So I just adapted the code from anthropic to openia, is not a big difference as you can see
+- https://markaicode.com/claude-sonnet-4-python-api-integration/
+
--- client.py
+++ client.py
@@ -1,6 +1,40 @@
 # Cliente: es el componente que mantiene la conexión con el servidor, y obtiene la información sobre como utilizarlo. 
 import asyncio
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-async def main():
-    # Aquí va la lógica del cliente
-    pass
+#load env variabless
+load_dotenv()
+
+class OpeniaGPT4ominiClient:
+    def __init__(self, model: str = "gpt-4o-mini"):
+        self.api_key = os.getenv('OPENIA_API_KEY')
+        if not self.api_key:
+            raise RuntimeError("Falta OPENAI_API_KEY en el entorno")
+        self.client = OpenAI(
+        api_key=self.api_key
+        )
+        
+        self.model = model
+
+    def send_message(self, message,  max_output_tokens = 500, store=True):
+        try:
+            response = self.client.responses.create(
+            model=self.model,
+            input=message,
+            max_output_tokens=max_output_tokens,
+            store=store,
+            )
+            return response.output_text
+        
+        except Exception as e: 
+            print("error: ", str(e))
+    
+
+
+chatgpt4mini = OpeniaGPT4ominiClient()
+print("====== ChatGPT 4o mini =============")
+question = input("Pregunta lo que quieras: ")
+response = chatgpt4mini.send_message(question )
+print(response)
