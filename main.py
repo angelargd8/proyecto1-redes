428cdd2a899ac94254036cf67184166c8771df2d
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2025-09-04 17:45:42 -0600

    feat: add /repo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #sssssa
 from client import *
 from server import *
-from mcpClient import commit_readme_in_existing_repo
+from mcpClient import commit_readme_in_existing_repo, create_repo_with_readme_and_commit
 import traceback
 
 def main():
@@ -52,6 +52,32 @@ def main():
                     print(f"Error al crear el repositorio: {e}")
                     print("error en el /readme", traceback.format_exc()) #
                 continue
+
+            elif question.startswith("/repo "):
+                try:
+                    _, rest = question.split("/repo ", 1)
+                    parts = [p.strip() for p in rest.split("|")]
+                    repo_path  = parts[0]
+                    readme_txt = parts[1] if len(parts) > 1 else "# README\n"
+                    commit_msg = parts[2] if len(parts) > 2 else "chore: init"
+
+                    create_repo_with_readme_and_commit(
+                        repo_path=repo_path,
+                        readme_text=readme_txt,
+                        commit_msg=commit_msg,
+                        allowed_dirs=[
+                            r"C:/Users/angel/Projects",
+                            r"C:/Users/angel/Desktop",
+                            r"C:/Users/angel/OneDrive/Documentos/.universidad/.2025/s2/redes/proyecto1-redes",
+                            r"C:/Users/angel/OneDrive/Documentos/.universidad/.2025/s2/redes",
+
+                        ],
+                    )
+                    print(f"Repositorio creado en {repo_path} con README y commit.")
+                except Exception as e:
+                    print(f"Error en /repo: {e}")
+                continue
+
             else:
                 response = svc.ask(session_id, question, max_output_tokens=100)
                 print(response)
